355fb5780eade891cc63c417ed028d865c25a364
RachelNolan <mail@rachelnolan.com>
2014-11-23 14:18:18 +0000

    More refactoring in Turn class, simplifying complex methods

--- src/Turn.js
+++ src/Turn.js
@@ -16,24 +16,54 @@ Turn.prototype.bowlTwo = function(pinsDown) {
 };
 
 Turn.prototype.recordScore = function() {
-	this.game.frameScores.push(this.scoreByBowl)
-	this.game.frameBonusPoints.push(this.bonusPoints)
+	this._recordFrameScoreToGame();
+	this._recordBonusScoreToGame();
+	
 };
 
 Turn.prototype.recordBonusPoints = function() {
 	if (this._isStrike()) {
-		this.bonusPoints = this._nextFrameBothBowls() * 2
+		this._awardDoubleNextFrame();
 	} 
-	else if (this._isSpare()) {
-		this.bonusPoints = this._nextFrameFirstBowl() * 2
-	}
-	else {
-		this.bonusPoints = 0
+	if (this._isSpare()) {
+		this._awardDoubleFirstBowlOfNextFrame();
 	}
 };
 
 
 // PRIVATE
+
+Turn.prototype._scorePinsHit = function(pinsDown) {
+	if (this._isStrike()) {
+		this._recordZeroForSecondBowl();
+	}
+	if (this._noMoreBowlsAllowed()) {
+		return undefined
+	}
+	this.score += pinsDown;	
+	this.scoreByBowl.push(pinsDown)
+	this.pinsStanding -= pinsDown;
+};
+
+Turn.prototype._recordFrameScoreToGame = function() {
+	this.game.frameScores.push(this.scoreByBowl)
+};
+
+Turn.prototype._recordBonusScoreToGame = function() {
+	this.game.frameBonusPoints.push(this.bonusPoints)
+};
+
+Turn.prototype._isStrikeOrSpare = function() {
+	return (this.score  === 10)
+};
+
+Turn.prototype._isStrike = function() {
+	return (this.scoreByBowl[0] === 10)
+};
+
+Turn.prototype._isSpare = function () {
+	return (this._isStrikeOrSpare() && !this._isStrike());
+}
 
 Turn.prototype._nextFrameNumber = function() {
 	return (this.game.turns.indexOf(this) + 1)
@@ -49,34 +79,20 @@ Turn.prototype._nextFrameBothBowls = function() {
 	return this.game.turns[i].score	
 };
 
-Turn.prototype._isStrikeOrSpare = function() {
-	return (this.score  === 10)
-};
-
-Turn.prototype._isStrike = function() {
-	return (this.scoreByBowl[0] === 10)
-};
-
-Turn.prototype._isSpare = function () {
-	return (this._isStrikeOrSpare() && !this._isStrike());
-}
-
-Turn.prototype._scorePinsHit = function(pinsDown) {
-	if (this._isStrike()) {
-		this.scoreByBowl.push(0)
-		// awardZeroForSecondBowl
-	}
-	if (this._noMoreBowlsAllowed()) {
-		return undefined
-	}
-	this.score += pinsDown;	
-	this.scoreByBowl.push(pinsDown)
-	this.pinsStanding -= pinsDown;
-};
-
-
 Turn.prototype._noMoreBowlsAllowed = function() {
 	return (this.scoreByBowl.length >= 2)
+};
+
+Turn.prototype._recordZeroForSecondBowl = function() {
+	this.scoreByBowl.push(0)
+};
+
+Turn.prototype._awardDoubleNextFrame = function() {
+	this.bonusPoints = this._nextFrameBothBowls() * 2
+};
+
+Turn.prototype._awardDoubleFirstBowlOfNextFrame = function() {
+	this.bonusPoints = this._nextFrameFirstBowl() * 2.
 };
 
 
